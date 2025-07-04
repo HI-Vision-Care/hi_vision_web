d2e8cbe1d2416819a5a117c89ca2b500abaf208f
Dang Khoa <ojtgrowth@sendo.vn>
2025-07-04 21:34:31 +0700

    Refactor component exports in index.ts and update tsconfig paths for better module resolution

--- components/index.ts
+++ components/index.ts
@@ -1,32 +1,8 @@
-export * from "./home/FeaturesSection";
-export * from "./home/HelpSection";
-export * from "./home/Footer";
-export * from "./home/HeaderHome";
-export * from "./home/HeroSection";
-
-export * from "./Chatbot/Gemini";
-
-export * from "./admin/header";
-export * from "./admin/sidebar";
-
-export * from "./appoinment/app-sidebar";
-export * from "./appoinment/appoinment-detail";
-export * from "./appoinment/appointments-list";
-export * from "./appoinment/dashboard-header";
-export * from "./appoinment/dashboard-overview";
-export * from "./appoinment/medical-record-form";
-export * from "./appoinment/medical-records-list";
-
-export * from "./auth/AuthContext";
-export * from "./auth/AuthForm";
-
-export * from "./partials/AccountFormModal";
-export * from "./partials/AppointmentEditModal";
-export * from "./partials/ARVModal";
-export * from "./partials/ConfirmDeleteModal";
-export * from "./partials/DoctorFormModal";
-export * from "./partials/HomeSwiper";
-export * from "./partials/LoginNav";
-export * from "./partials/PatientFormModal";
-
-export * from "./doctor/DoctorDashboardClient";
+// src/components/index.ts
+export * from "./admin";
+export * from "./auth";
+export * from "./appoinment";
+export * from "./Chatbot";
+export * from "./partials";
+export * from "./doctor";
+export * from "./home";
